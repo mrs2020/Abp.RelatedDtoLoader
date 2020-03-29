78c51d2cf815f10b07f4995b785925e279ef4d3a
itryan <itryan@users.noreply.github.com>
2020-03-29 23:21:48 +0800

    feat: add loader configuration

--- test/EasyAbp.Abp.RelatedDtoLoader.IntegratedTests/IntegerKey/MyIntegerRelatedDtoLoaderProfile.cs
+++ test/EasyAbp.Abp.RelatedDtoLoader.IntegratedTests/IntegerKey/MyIntegerRelatedDtoLoaderProfile.cs
@@ -8,9 +8,9 @@ namespace EasyAbp.Abp.RelatedDtoLoader.Tests.IntegratedTests
         public MyIntegerRelatedDtoLoaderProfile() 
             : base()
         {
-            AddModule<RelatedDtoLoaderTestBaseModule>();
+            base.UseRepositoryLoader<IntProductDto, IntProduct, int>();
 
-            CreateRule<IntProductDto, IntProduct, int>();
+            EnableTargetDto<IntOrderDto>();
         }
     }
 }
